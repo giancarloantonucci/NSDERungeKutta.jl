d56027244c73ba9fa493d1d7f2873469e987db33
giancarloantonucci <giancarlo.antonucci@icloud.com>
2023-07-21 16:29:12 +0200

    Changed notation and a few error messages

--- src/erk/adaptivestep.jl
+++ src/erk/adaptivestep.jl
@@ -4,26 +4,30 @@ function adaptivestep!(cache::ExplicitRungeKuttaCache, solution::AbstractRungeKu
     @↓ tableau, stepsize = solver
     @↓ s, b, p, d, q = tableau
     @↓ h = stepsize
-    @↓ ϵₐ ← atol, ϵᵣ ← rtol, M ← nits = adaptive
+    @↓ atol, rtol, nits = adaptive
+    # Compute the error estimate and target error
     zero!(v)
     for i = 1:s
         @. v += (b[i] - d[i]) * k[i]
     end
-    ϵ = ϵₐ + norm(u[n]) * ϵᵣ
+    ϵ = atol + norm(u[n]) * rtol
     err = hairernorm(v)
+    # Adjust the step-size based on the error and the order of the method
     h *= max(0.5, min(2.0, (0.35 * ϵ / err) ^ (1 / (min(p, q) + 1))))
     if h ≈ zero(h)
-        error("Stepsize `h` has become too small at `t = $(t[n])`.")
+        error("Step-size `h` too small at `t = $(t[n])`.")
     end
-    if err < ϵ || m ≥ M
+    # Check if the error is within the tolerance or if the maximum iterations reached
+    if err < ϵ || m ≥ nits
         m = 1
         n += 1
-        if m ≥ M
-            println("Adaptive loop has reached `nits` at `t = $(t[n])`.")
+        if m ≥ nits
+            println("Maximum number of iterations `nits` hit at `t = $(t[n])`.")
         end
     else
         m += 1
     end
+    # Update the stepsize and the cache
     @↑ cache = n, m
     @↑ stepsize = h
     return solution
